019b0062121b3fdc18ea9ebbf99658e85702650e
Madaminov2002 <suhrobjonmadaminov37@gmail.com>
2024-07-29 15:08:20 +0500

    first commit

---
+++ src/main/java/org/example/arapp/ArAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.arapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/arapp/ArAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.arapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
